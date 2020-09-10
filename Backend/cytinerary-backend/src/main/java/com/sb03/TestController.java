4a8ede0d34a422dd280f29faf879c0ea0902c544
Michael Andrews <maa3@iastate.edu>
2020-09-10 15:07:13 -0500

    Adjust Maven dependencies and configuration to allow for deployment to Tomcat

--- Backend/cytinerary-backend/src/main/java/com/sb03/TestController.java
+++ Backend/cytinerary-backend/src/main/java/com/sb03/TestController.java
@@ -1,14 +1,15 @@
 package com.sb03;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 class TestController {
 	
 	@RequestMapping("/")
 	public String res() {
-		return "Hello";
+		return "index.html";
 	}
 	
 }
---
+++ Backend/cytinerary-backend/src/main/resources/static/precache-manifest.5b9c46d0dc544bdcb5dffadfdc31b646.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30be5b61088bd353d33d510b40c51610",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7660baac9d0c0f2c95d7",
+    "url": "/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "90e52c57d37746d906fc",
+    "url": "/static/js/2.763b34e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.763b34e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7660baac9d0c0f2c95d7",
+    "url": "/static/js/main.0eaced7b.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
