9016f28ce97d11c3536f4386e8861f6a3fa037a5
lewiss <lewiss@iastate.edu>
2020-09-14 10:01:42 -0500

    Add child elements for the NewEventModal
    Add unformatted base text inputs and dropdowns.

--- Frontend/my-app/src/components/newEventModal.tsx
+++ Frontend/my-app/src/components/newEventModal.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { Dialog, Button } from '@material-ui/core/';
+import { Dialog, DialogTitle, DialogContent, Button, TextField, NativeSelect } from '@material-ui/core/';
 
 interface IProps {
   //onSubmit: string,
@@ -7,6 +7,8 @@ interface IProps {
   onClose: () => void,
 }
 interface IState {
+  tasks?: string[],
+  currentInput: string,
 }
 
 class NewEventModal extends React.Component<IProps, IState> {
@@ -20,8 +22,36 @@ class NewEventModal extends React.Component<IProps, IState> {
     return(
       <div>
         <Dialog maxWidth = {'md'} open = {this.props.open} onBackdropClick = {() => {this.props.onClose()}}>
+          <DialogTitle>Create an event</DialogTitle>
+              <DialogContent>
+                <div style = {{flexDirection: 'column', }}>
+                  <div style = {{display: 'flex'}}> <TextField id = "nameInput" label = "Name" variant="outlined"/> </div>
+                  <div style = {{display: 'flex'}}> <TextField id = "descriptionInput" label = "Description" variant="outlined"/> </div>
+                  <div id = "time-selector">
+                    <text>From</text>
+                    <TextField label = "" variant="outlined"/>
+                    <NativeSelect>
+                      <option value = {"am"}>AM</option>
+                      <option value = {"pm"}>PM</option>
+                    </NativeSelect>
+                    <text>To</text>
+                    <TextField label = "" variant="outlined"/>
+                    <NativeSelect>
+                      <option value = {"am"}>AM</option>
+                      <option value = {"pm"}>PM</option>
+                    </NativeSelect>
+                  </div>
+                  <div style = {{display: 'flex'}}>
+                    <TextField id = "descriptionInput" label = "Label" variant="outlined"/>
+                    {/*Will Need to add color block for color selection*/}
+                  </div>
+                  <div>
                   <Button onClick = {() => {this.props.onClose()}} style = {{color:'black'}}>Cancel</Button>
                   <Button onClick = {() => {this.props.onClose()}} style = {{color:'black'}}>Create</Button>
+                  </div>
+                </div>
+              </DialogContent>
+
         </Dialog>
       </div>
     );
