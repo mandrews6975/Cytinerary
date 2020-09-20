3e156faab9208e4e802812c181041ad6afc0c326
Michael Andrews <maa3@iastate.edu>
2020-09-20 12:08:27 -0500

    Implement backend structure for user-based requests/queries

---
+++ Backend/cytinerary-backend/src/main/java/com/sb03/UserController.java
@@ -0,0 +1,53 @@
+package com.sb03;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Collection;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+import com.sb03.modal.User;
+
+@RestController
+@CrossOrigin(origins = "*")
+public class UserController {
+
+  @Autowired
+  private UserRepository userRepository;
+
+	@GetMapping("/getUsers")
+  public @ResponseBody Collection<User> getUsers() {
+  return userRepository.getUsers();
+  }
+
+  @Transactional
+  @PostMapping("/addUser")
+  public @ResponseBody String addUser(@RequestBody Map<String, Object> payload) {
+    userRepository.addUser((String) payload.get("netId"), (String) payload.get("LastName"), (String) payload.get("FirstName"), (String) payload.get("password"));
+    return ((String) payload.get("userId") + "added");
+  }
+
+  @Transactional
+  @PostMapping("/deleteUser")
+  public @ResponseBody String deleteUser(@RequestBody Map<String, Object> payload) {
+    userRepository.deleteUser((String) payload.get("userId"));
+    return ((String) payload.get("userId") + "deleted");
+  }
+
+  @PostMapping("/getUser")
+  public @ResponseBody Collection<User> getUser(@RequestBody Map<String, Object> payload) {
+    return userRepository.getUser((String) payload.get("UserId"));
+  }
+}
