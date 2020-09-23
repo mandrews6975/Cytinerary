a6d64fa488550a3b141ac9caaa8e62148a7ab001
lewiss <lewiss@iastate.edu>
2020-09-23 11:33:34 -0500

    Update backend application.properties to include specified timezone
    Add static build files to the .gitignore

--- Backend/cytinerary-backend/src/main/java/com/sb03/EventController.java
+++ Backend/cytinerary-backend/src/main/java/com/sb03/EventController.java
@@ -1,26 +1,21 @@
 package com.sb03;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RequestParam;
+
 
 import java.util.Collection;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
+
 
 import com.sb03.modal.Event;
 import com.sb03.repository.EventRepository;
-import com.sb03.service.EventService;
 
 
 @RestController
