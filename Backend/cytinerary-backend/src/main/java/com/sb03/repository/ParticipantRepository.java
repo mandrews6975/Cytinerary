2e5a9ab3371d530bea760c9fe3b3775caf6f7a11
mandrews6975 <m.andrews6975@gmail.com>
2020-10-18 18:58:18 -0500

    Retrieve event info from server and fill input fields with said info in dialog window

--- Backend/cytinerary-backend/src/main/java/com/sb03/repository/ParticipantRepository.java
+++ Backend/cytinerary-backend/src/main/java/com/sb03/repository/ParticipantRepository.java
@@ -18,8 +18,10 @@ public interface ParticipantRepository extends CrudRepository<Participant, Long>
   @Query(value="delete from participants where eventId = ?1 and participant = ?2 ", nativeQuery = true)
   void removeParticipant(String eventId, String participant);
 
-
   @Query(value="select * from participants where eventId = ?1", nativeQuery = true)
   Collection<Participant> getParticipants(String eventId);
 
+  @Query(value="select user.userId, user.netId, user.lastName, user.firstName from users user inner join participants on user.userId = participants.participant where participants.eventId = ?1", nativeQuery = true)
+  Collection<Object> getParticipantsSecure(String sharerId);
+  
 }
--- Frontend/my-app/src/components/dialog_windows/EditEventDialogWindow.tsx
+++ Frontend/my-app/src/components/dialog_windows/EditEventDialogWindow.tsx
@@ -64,10 +64,20 @@ const theme = createMuiTheme({
   }
 });
 
-const sharerId = '111';
+const creatorId = '111';
 
 function EditEventDialogWindow(props: Props) {
 
+  const [originalEvent, setOriginalEvent] = useState<Event>({
+    eventId: props.eventId,
+    creator: creatorId,
+    name: '',
+    description: '',
+    location: '',
+    startTime: new Date(),
+    endTime: new Date(),
+    label: ''
+  });
   const [currentlyShared, setCurrentlyShared] = useState<{ userId: string, name: string, email: string }[]>([]);
   const [titleInput, setTitleInput] = useState<string>('');
   const [startTime, setStartTime] = useState<Date>(new Date());
@@ -75,35 +85,71 @@ function EditEventDialogWindow(props: Props) {
   const [locationInput, setLocationInput] = useState<string>('');
   const [descriptionInput, setDescriptionInput] = useState<string>('');
 
-  // useEffect(() => getSharedUsers(sharerId), []);
+  useEffect(() => getEvent(creatorId, props.eventId), []);
+  useEffect(() => getParticipants(props.eventId), []);
 
-  // function getSharedUsers(sharerId: string) {
-  //   try {
-  //     fetch('/getSharedUsers', {
-  //       method: 'POST',
-  //       headers: {
-  //         Accept: 'application/json',
-  //         'Content-Type': 'application/json',
-  //       },
-  //       body: JSON.stringify({
-  //         sharerId: sharerId
-  //       }),
-  //     }).then((response) => response.json())
-  //       .then((json) => {
-  //         let newCurrentlyShared: { userId: string, name: string, email: string }[] = [];
-  //         json.forEach((sharee: string[]) => {
-  //           newCurrentlyShared.push({
-  //             userId: sharee[0],
-  //             name: sharee[3] + ' ' + sharee[2],
-  //             email: sharee[1] + '@iastate.edu'
-  //           });
-  //         });
-  //         setCurrentlyShared(newCurrentlyShared);
-  //       });
-  //   } catch (err) {
-  //     console.log(err);
-  //   }
-  // }
+  function getEvent(creatorId: string, eventId: string) {
+    try {
+      fetch('/getEvent', {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          creatorId: creatorId,
+          eventId: eventId
+        }),
+      }).then((response) => response.json())
+        .then((json) => {
+          setTitleInput(json[0].name);
+          setStartTime(json[0].startTime);
+          setEndTime(json[0].endTime);
+          setLocationInput(json[0].location);
+          setDescriptionInput(json[0].description);
+          setOriginalEvent({
+            eventId: props.eventId,
+            creator: creatorId,
+            name: json[0].name,
+            description: json[0].description,
+            location: json[0].location,
+            startTime: json[0].startTime,
+            endTime: json[0].endTime,
+            label: ''
+          })
+        });
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  function getParticipants(eventId: string) {
+    try {
+      fetch('/getParticipantsSecure', {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          eventId: eventId
+        }),
+      }).then((response) => response.json())
+        .then((json) => {
+          let newCurrentlyShared: { userId: string, name: string, email: string }[] = [];
+          json.forEach((sharee: string[]) => {
+            newCurrentlyShared.push({
+              userId: sharee[0],
+              name: sharee[3] + ' ' + sharee[2],
+              email: sharee[1] + '@iastate.edu'
+            });
+          });
+          setCurrentlyShared(newCurrentlyShared);
+        });
+    } catch (err) {
+      console.log(err);
+    }
+  }
 
   return (
     <Dialog
@@ -115,158 +161,178 @@ function EditEventDialogWindow(props: Props) {
         zIndex: 1
       }}
     >
-      <ThemeProvider theme={theme}>
-        <DialogTitle>Event Details</DialogTitle>
-        <DialogContent>
-          <div
+      <DialogTitle>Event Details</DialogTitle>
+      <DialogContent>
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'space-between'
+          }}
+        >
+          <TextField
+            variant='standard'
+            label='Title'
             style={{
-              display: 'flex',
-              flexDirection: 'row',
-              alignItems: 'center',
-              justifyContent: 'space-between'
+              width: '30%'
             }}
+            value={titleInput || ''}
+            onChange={((event) => {
+              setTitleInput(event.target.value);
+            })}
+            key='titleInput'
+          />
+          <Button
+            variant='outlined'
+            color='inherit'
+            onClick={() => { }}
           >
-            <TextField
-              variant='standard'
-              label='Title'
-              style={{
-                width: '30%'
-              }}
-            />
-            <Button
-              variant='outlined'
-              color='inherit'
-              onClick={() => { }}
-            >
-              {dateFormat(startTime, 'mm/dd/yy hh:MM TT')}
-            </Button>
-            <Typography variant='subtitle1'>
-              to
+            {dateFormat(startTime, 'mm/dd/yy hh:MM TT')}
+          </Button>
+          <Typography variant='subtitle1'>
+            to
             </Typography>
-            <Button
-              variant='outlined'
-              color='inherit'
-              onClick={() => { }}
-            >
-              {dateFormat(endTime, 'mm/dd/yy hh:MM TT')}
-            </Button>
-            <TextField
-              variant='standard'
-              label='Location'
-              style={{
-                width: '30%'
-              }}
-            />
-          </div>
-          <Divider
+          <Button
+            variant='outlined'
+            color='inherit'
+            onClick={() => { }}
+          >
+            {dateFormat(endTime, 'mm/dd/yy hh:MM TT')}
+          </Button>
+          <TextField
+            variant='standard'
+            label='Location'
             style={{
-              marginTop: '10px',
-              marginBottom: '10px',
+              width: '30%'
             }}
+            value={locationInput || ''}
+            onChange={((event) => {
+              setLocationInput(event.target.value);
+            })}
+            key='locationInput'
           />
+        </div>
+        <Divider
+          style={{
+            marginTop: '10px',
+            marginBottom: '10px',
+          }}
+        />
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'row',
+            alignItems: 'flex-start'
+          }}
+        >
           <div
             style={{
               display: 'flex',
-              flexDirection: 'row',
-              alignItems: 'flex-start'
+              flexDirection: 'column',
+              alignItems: 'center',
+              justifyContent: 'center',
+              flex: 7,
+              height: window.innerHeight * 0.3075,
+              marginRight: '10px'
+            }}
+          >
+            <TextField
+              variant='filled'
+              label='Description'
+              style={{
+                width: '100%',
+              }}
+              multiline
+              rows={13}
+              rowsMax={13}
+              value={descriptionInput || ''}
+              onChange={((event) => {
+                setDescriptionInput(event.target.value);
+              })}
+              key='descriptionInput'
+            />
+          </div>
+          <div
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+              justifyContent: 'center',
+              flex: 3,
+              height: window.innerHeight * 0.3075,
             }}
           >
             <div
               style={{
                 display: 'flex',
-                flexDirection: 'column',
+                flexDirection: 'row',
                 alignItems: 'center',
-                justifyContent: 'center',
-                flex: 7,
-                height: window.innerHeight * 0.3075,
-                marginRight: '10px'
+                justifyContent: 'space-between',
+                flex: 1,
+                width: '100%'
               }}
             >
-              <TextField
-                variant='filled'
-                label='Description'
-                style={{
-                  width: '100%',
-                }}
-                multiline
-                rows={13}
-                rowsMax={13}
-              />
+              <Typography variant='subtitle1'>
+                Participants
+                </Typography>
+              <IconButton>
+                <Edit />
+              </IconButton>
             </div>
             <div
               style={{
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center',
-                justifyContent: 'center',
-                flex: 3,
-                height: window.innerHeight * 0.3075,
+                overflowY: 'scroll',
+                height: window.innerHeight * 0.25,
+                width: '100%'
               }}
             >
-              <div
-                style={{
-                  display: 'flex',
-                  flexDirection: 'row',
-                  alignItems: 'center',
-                  justifyContent: 'space-between',
-                  flex: 1,
-                  width: '100%'
-                }}
-              >
-                <Typography variant='subtitle1'>
-                  Participants
-                </Typography>
-                <IconButton>
-                  <Edit />
-                </IconButton>
-              </div>
-              <div
-                style={{
-                  overflowY: 'scroll',
-                  height: window.innerHeight * 0.25,
-                  width: '100%'
-                }}
-              >
-                <List>
-                  {currentlyShared.map((
-                    person: { userId: string, name: string, email: string },
-                    index: number,
-                    array: { userId: string, name: string, email: string }[]
-                  ) => (
-                      <ShareDialogListItem
-                        name={person.name}
-                        email={person.email}
-                        color='red'
-                        key={`${person.name}-${person.email}-${index}-shared`}
-                      />
-                    ))}
-                </List>
-              </div>
+              <List>
+                {currentlyShared.map((
+                  person: { userId: string, name: string, email: string },
+                  index: number,
+                  array: { userId: string, name: string, email: string }[]
+                ) => (
+                    <ShareDialogListItem
+                      name={person.name}
+                      email={person.email}
+                      color='red'
+                      key={`${person.name}-${person.email}-${index}-shared`}
+                    />
+                  ))}
+              </List>
             </div>
           </div>
-          <Divider
-            style={{
-              marginTop: '10px'
-            }}
-          />
-        </DialogContent>
-        <DialogActions>
-          <Button
-            variant='contained'
-            color='primary'
-            onClick={() => props.onClose()}
-          >
-            Cancel
+        </div>
+        <Divider
+          style={{
+            marginTop: '10px'
+          }}
+        />
+      </DialogContent>
+      <DialogActions>
+        <Button
+          variant='contained'
+          color='primary'
+          onClick={() => props.onClose()}
+        >
+          Cancel
           </Button>
-          <Button
-            variant='contained'
-            color='primary'
-            onClick={() => props.onClose()}
-          >
-            Update
-          </Button>
-        </DialogActions>
-      </ThemeProvider>
+        <Button
+          variant='contained'
+          color='primary'
+          onClick={() => props.onClose()}
+          disabled={!(
+            originalEvent.name !== titleInput ||
+            originalEvent.startTime !== startTime ||
+            originalEvent.endTime !== endTime ||
+            originalEvent.location !== locationInput ||
+            originalEvent.description !== descriptionInput
+          )}
+        >
+          Update
+        </Button>
+      </DialogActions>
     </Dialog>
   );
 
