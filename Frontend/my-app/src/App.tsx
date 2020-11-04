745d3c71a951e3956bf8532697aafff7082cd040
lewiss <lewiss@iastate.edu>
2020-11-03 18:44:43 -0600

    Add frontend fetch request for verifying login credentials Update LoginScreen to send request on click of the login button. Update LoginScreen to use material UI Styling.

--- Frontend/my-app/src/App.tsx
+++ Frontend/my-app/src/App.tsx
@@ -15,17 +15,6 @@ function App() {
   return (
     <Router>
           <div>
-            <nav>
-              <ul>
-                <li>
-                  <Link to="/">Login</Link>
-                </li>
-                <li>
-                  <Link to="/home">Home</Link>
-                </li>
-              </ul>
-            </nav>
-
             {/* A <Switch> looks through its children <Route>s and
                 renders the first one that matches the current URL. */}
             <Switch>
--- Frontend/my-app/src/components/screens/LoginScreen.tsx
+++ Frontend/my-app/src/components/screens/LoginScreen.tsx
@@ -4,11 +4,19 @@ import red from '@material-ui/core/colors/red';
 import grey from '@material-ui/core/colors/grey';
 import yellow from '@material-ui/core/colors/yellow';
 
+import {TextField, Button, Typography} from '@material-ui/core'
+
 import {
   Person,
   Lock
 } from '@material-ui/icons'
 
+interface IState {
+  username: string,
+  password: string,
+}
+
+interface IProps {}
 
 const colors = ({
   palette: {
@@ -27,38 +35,63 @@ const colors = ({
   }
 });
 
-class App extends React.Component{
+class App extends React.Component<IProps, IState>{
+
+  constructor(props: IProps){
+    super(props);
+    this.state = {
+      username: '',
+      password: ''
+    }
+  }
+
+  sendRequest() {
+    fetch('/authenticateUser', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        netId: this.state.username,
+        password: this.state.password
+      }),
+  }).then((response) => response.text())
+  .then((text) => {
+    alert(text)
+  });
+  }
+
   render() {
+
     return (
-      <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', backgroundColor: colors.palette.background.default, minHeight: window.innerHeight,
+      <div style={{
+        height: window.innerHeight,
+        display: 'flex',
+        backgroundColor: red[100],
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center'
       }}>
-      <h1 style={{position:'fixed', marginRight:564, marginBottom:800, fontFamily: 'monospace'}}>
-        Cytinerary
-      </h1>
-        <view style={{position: 'fixed', backgroundColor: colors.palette.primary.default, borderStyle: 'solid', borderColor: colors.palette.secondary.default, width:700, height: 700}}>
-        <h1 style={{position:'fixed', marginLeft:250, fontFamily: 'serif'}}>
-          Sign In
-        </h1>
-          <h3 style={{marginTop:100, marginLeft:250, fontFamily: 'serif'}}>
-            Username:
-          </h3>
-          <span style={{height: 20, width: 20, backgroundColor: colors.palette.tertiary.default, border:'solid', borderWidth: 1, borderRadius:50,display: 'inline-block', position:'fixed', marginLeft:215, marginTop:3}}>
-            <Person style={{height: 15, width: 15, position:'fixed', color: 'inherit', marginLeft:2, marginTop:2}}/>
-          </span>
-          <input style={{width:170, marginLeft:250}}>
-          </input>
-          <h3 style={{marginTop:50, marginLeft:250, fontFamily: 'serif'}}>
-            Password:
-          </h3>
-          <span style={{height: 20, width: 20, backgroundColor: colors.palette.tertiary.default, border:'solid', borderWidth: 1, borderRadius:50,display: 'inline-block', position:'fixed', marginLeft:215, marginTop:3}}>
-            <Lock style={{height: 15, width: 15, position:'fixed', color: 'inherit', marginLeft:2, marginTop:2}}/>
-          </span>
-          <input style={{width:170, marginLeft:250}}>
-          </input>
-          <button>
+        <div style={{
+          height: window.innerHeight,
+          width: window.innerWidth * 2 / 3,
+          display: 'flex',
+          backgroundColor: yellow[50],
+          flexDirection: 'column',
+          alignItems: 'center',
+          borderLeft: '1px solid black',
+          borderRight: '1px solid black'
+        }}>
+          <Typography variant='h2' style={{marginBottom: '100px', marginTop: '50px'}}>
+            Cytinerary
+          </Typography>
+          <TextField onChange = {(event) => this.setState({username: event.target.value})} label = {"netId"} style={{marginBottom: '20px'}}/>
+          <TextField onChange = {(event) => this.setState({password: event.target.value})} label = {"Password"} style={{marginBottom: '20px'}} inputProps={{type: 'password'}}/>
+          <Button onClick = {() => {this.sendRequest()}}>
             Login
-          </button>
-        </view>
+          </Button>
+        </div>
       </div>
     );
   }
