a110dec390561eff6c4b18230792963b4d4c6422
lewiss <lewiss@iastate.edu>
2020-11-16 14:51:39 -0600

    Modify base controller to handle any of the router paths
    This should fix the problem with the paths redirecting to white label 
    error on ssg server.

--- Backend/cytinerary-backend/src/main/java/com/sb03/TestController.java
+++ Backend/cytinerary-backend/src/main/java/com/sb03/TestController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 class TestController {
 
-	@RequestMapping("/")
+	@RequestMapping({"/", "/home", "/sharedschedules", "/login"})
 	public String res() {
 		return "index.html";
 	}
